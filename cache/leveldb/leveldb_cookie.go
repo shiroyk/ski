ce3860ef328d97996712f5a283396a8a98c1045e
shiroyk <kumoocat@gmail.com>
2023-01-18 17:23:32 +0800

    Improve leveldb cookie

--- cache/leveldb/leveldb_cookie.go
+++ cache/leveldb/leveldb_cookie.go
@@ -3,7 +3,6 @@ package leveldb
 import (
 	"net/http"
 	"net/url"
-	"sync"
 
 	"github.com/shiroyk/cloudcat/utils"
 	"github.com/syndtr/goleveldb/leveldb"
@@ -11,21 +10,16 @@ import (
 
 // Cookie is an implementation of cache.Cookie that stores http.Cookie in leveldb.DB.
 type Cookie struct {
-	mu sync.RWMutex
 	Db *leveldb.DB
 }
 
 // SetCookieString handles the receipt of the cookies strung in a reply for the given URL.
 func (c *Cookie) SetCookieString(u *url.URL, cookies string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	c.SetCookies(u, utils.ParseCookie(cookies))
 }
 
 // CookieString returns the cookies string to send in a request for the given URL.
 func (c *Cookie) CookieString(u *url.URL) string {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
 	value, err := c.Db.Get([]byte(u.Host), nil)
 	if err != nil {
 		return ""
@@ -35,22 +29,16 @@ func (c *Cookie) CookieString(u *url.URL) string {
 
 // DeleteCookie handles the receipt of the cookies in a reply for the given URL.
 func (c *Cookie) DeleteCookie(u *url.URL) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	_ = c.Db.Delete([]byte(u.Host), nil)
 }
 
 // SetCookies handles the receipt of the cookies in a reply for the given URL.
 func (c *Cookie) SetCookies(u *url.URL, cookies []*http.Cookie) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	_ = c.Db.Put([]byte(u.Host), []byte(utils.CookieToString(cookies)), nil)
 }
 
 // Cookies returns the cookies to send in a request for the given URL.
 func (c *Cookie) Cookies(u *url.URL) []*http.Cookie {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
 	return utils.ParseCookie(c.CookieString(u))
 }
 
