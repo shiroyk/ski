f6f5add190b2f9ba23d6758f508870cd403b1b85
shiroyk <kumoocat@gmail.com>
2023-02-15 09:10:29 -0600

    Move format

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -1,12 +1,10 @@
 package analyzer
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/shiroyk/cloudcat/parser"
 	"github.com/shiroyk/cloudcat/schema"
-	"github.com/spf13/cast"
 )
 
 // Analyzer context analyzer
@@ -136,54 +134,4 @@ func (analyzer *Analyzer) processInit(ctx *parser.Context, s *schema.Schema, con
 		ctx.Logger().Error("process init failed", err)
 	}
 	return []string{element}
-}
-
-// FormatHandler schema property formatter
-type FormatHandler interface {
-	// Format the data to the given schema.SchemaType
-	Format(data any, format schema.Type) (any, error)
-}
-
-type defaultFormatHandler struct{}
-
-// Format the data to the given schema.SchemaType
-func (f defaultFormatHandler) Format(data any, format schema.Type) (any, error) {
-	switch data := data.(type) {
-	case string:
-		switch format {
-		case schema.StringType:
-			return data, nil
-		case schema.IntegerType:
-			return cast.ToIntE(data)
-		case schema.NumberType:
-			return cast.ToFloat64E(data)
-		case schema.BooleanType:
-			return cast.ToBoolE(data)
-		case schema.ArrayType:
-			slice := make([]any, 0)
-			if err := json.Unmarshal([]byte(data), &slice); err != nil {
-				return nil, err
-			}
-			return slice, nil
-		case schema.ObjectType:
-			object := make(map[string]any, 0)
-			if err := json.Unmarshal([]byte(data), &object); err != nil {
-				return nil, err
-			}
-			return object, nil
-		}
-	case []string:
-		slice := make([]any, len(data))
-		for i, o := range data {
-			slice[i], _ = f.Format(o, format)
-		}
-		return slice, nil
-	case map[string]any:
-		maps := make(map[string]any, len(data))
-		for k, v := range data {
-			maps[k], _ = f.Format(v, format)
-		}
-		return maps, nil
-	}
-	return data, fmt.Errorf("unexpected type %T", data)
 }
---
+++ analyzer/format_test.go
@@ -0,0 +1,39 @@
+package analyzer
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/shiroyk/cloudcat/schema"
+)
+
+func TestFormat(t *testing.T) {
+	formatter := new(defaultFormatHandler)
+	testCases := []struct {
+		data any
+		typ  schema.Type
+		want any
+	}{
+		{"1", schema.StringType, "1"},
+		{"2.1", schema.NumberType, 2.1},
+		{"3", schema.IntegerType, 3},
+		{"1", schema.BooleanType, true},
+		{`{"k":"v"}`, schema.ObjectType, map[string]any{"k": "v"}},
+		{`[1,2]`, schema.ArrayType, []any{1.0, 2.0}},
+		{[]string{"1", "2"}, schema.IntegerType, []any{1, 2}},
+		{map[string]any{"k": "1"}, schema.IntegerType, map[string]any{"k": 1}},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			got, err := formatter.Format(testCase.data, testCase.typ)
+			if err != nil {
+				t.Error(err)
+			}
+			if !reflect.DeepEqual(got, testCase.want) {
+				t.Errorf("got %v, want %v", got, testCase.want)
+			}
+		})
+	}
+}
