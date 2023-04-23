4f791183a7aaa66ee842b36de5b605ebffe019ca
shiroyk <kumoocat@gmail.com>
2023-04-23 11:21:47 +0800

    feat: parser js auto format result

--- parsers/js/js.go
+++ parsers/js/js.go
@@ -2,7 +2,9 @@
 package js
 
 import (
-	js2 "github.com/shiroyk/cloudcat/core/js"
+	"encoding/json"
+
+	"github.com/shiroyk/cloudcat/core/js"
 	"github.com/shiroyk/cloudcat/plugin"
 	"github.com/shiroyk/cloudcat/plugin/parser"
 	"github.com/spf13/cast"
@@ -20,42 +22,77 @@ func init() {
 // GetString gets the string of the content with the given arguments.
 // returns the string result.
 func (p *Parser) GetString(ctx *plugin.Context, content any, arg string) (ret string, err error) {
-	str, err := runScript(ctx, content, arg)
-	if err != nil {
-		return
-	}
-	return cast.ToStringE(str)
+	return getString(ctx, content, arg)
 }
 
 // GetStrings gets the strings of the content with the given arguments.
 // returns the slice of string result.
 func (p *Parser) GetStrings(ctx *plugin.Context, content any, arg string) (ret []string, err error) {
-	str, err := runScript(ctx, content, arg)
-	if err != nil {
-		return
-	}
-	return cast.ToStringSliceE(str)
+	return getStrings(ctx, content, arg)
 }
 
 // GetElement gets the element of the content with the given arguments.
 // returns the string result.
 func (p *Parser) GetElement(ctx *plugin.Context, content any, arg string) (string, error) {
-	return p.GetString(ctx, content, arg)
+	return getString(ctx, content, arg)
 }
 
 // GetElements gets the elements of the content with the given arguments.
 // returns the slice of string result.
 func (p *Parser) GetElements(ctx *plugin.Context, content any, arg string) ([]string, error) {
-	return p.GetStrings(ctx, content, arg)
+	return getStrings(ctx, content, arg)
 }
 
-func runScript(ctx *plugin.Context, content any, script string) (any, error) {
-	result, err := js2.Run(ctx, js2.Program{Code: script, Args: map[string]any{
+func getString(ctx *plugin.Context, content any, script string) (ret string, err error) {
+	result, err := js.Run(ctx, js.Program{Code: script, Args: map[string]any{
+		"content": content,
+	}})
+	if err != nil {
+		return ret, err
+	}
+
+	value, err := js.Unwrap(result)
+	if err != nil {
+		return ret, err
+	}
+
+	if maps, ok := value.(map[string]any); ok {
+		bytes, err := json.Marshal(maps)
+		if err != nil {
+			return ret, err
+		}
+		return string(bytes), nil
+	}
+
+	return cast.ToStringE(value)
+}
+
+func getStrings(ctx *plugin.Context, content any, script string) (ret []string, err error) {
+	result, err := js.Run(ctx, js.Program{Code: script, Args: map[string]any{
 		"content": content,
 	}})
 	if err != nil {
 		return nil, err
 	}
 
-	return js2.Unwrap(result)
+	value, err := js.Unwrap(result)
+	if err != nil {
+		return nil, err
+	}
+
+	slice, ok := value.([]any)
+	if !ok {
+		slice = []any{value}
+	}
+
+	ret = make([]string, len(slice))
+	for i, v := range slice {
+		if s, ok := v.(string); ok {
+			ret[i] = s
+		} else {
+			bytes, _ := json.Marshal(v)
+			ret[i] = string(bytes)
+		}
+	}
+	return
 }
--- parsers/js/js_test.go
+++ parsers/js/js_test.go
@@ -31,31 +31,52 @@ func TestParser(t *testing.T) {
 }
 
 func TestGetString(t *testing.T) {
-	str, err := jsParser.GetString(ctx, "a", `(async () => content + 1)()`)
-	if err != nil {
-		t.Fatal(err)
+	{
+		str, err := jsParser.GetString(ctx, "a", `(async () => content + 1)()`)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, "a1", str)
 	}
 
-	assert.Equal(t, "a1", str)
+	{
+		str, err := jsParser.GetString(ctx, "", `(async () => ({"test":"1"}))()`)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.JSONEq(t, `{"test":"1"}`, str)
+	}
 }
 
 func TestGetStrings(t *testing.T) {
-	str, err := jsParser.GetStrings(ctx, `["a1"]`,
-		`new Promise((r, j) => {
-				let s = JSON.parse(content);
-		   		s.push('a2');
-				r(s)
-		   });`)
-	if err != nil {
-		t.Fatal(err)
+	{
+		str, err := jsParser.GetStrings(ctx, `["a1"]`,
+			`new Promise((r, j) => {
+					let s = JSON.parse(content);
+					s.push('a2');
+					r(s)
+				});`)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, []string{"a1", "a2"}, str)
 	}
 
-	assert.Equal(t, []string{"a1", "a2"}, str)
+	{
+		str, err := jsParser.GetStrings(ctx, "", `[{"foo":"1"}, {"bar":"1"}, 19]`)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, []string{`{"foo":"1"}`, `{"bar":"1"}`, "19"}, str)
+	}
 }
 
 func TestGetElement(t *testing.T) {
-	ele, err := jsParser.GetElement(ctx, ``,
-		`cat.setVar('size', 1 + 2);cat.getVar('size');`)
+	ele, err := jsParser.GetElement(ctx, ``, `cat.setVar('size', 1 + 2);cat.getVar('size');`)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -65,8 +86,7 @@ func TestGetElement(t *testing.T) {
 
 func TestGetElements(t *testing.T) {
 	t.Parallel()
-	ele, err := jsParser.GetElements(ctx, ``,
-		`[1, 2]`)
+	ele, err := jsParser.GetElements(ctx, ``, `[1, 2]`)
 	if err != nil {
 		t.Fatal(err)
 	}
