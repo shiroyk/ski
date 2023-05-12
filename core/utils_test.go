100b3e67c85506a201ac92e50221b679e5868cff
shiroyk <kumoocat@gmail.com>
2023-05-12 05:49:31 -0600

    test: fix utils test case

--- core/utils_test.go
+++ core/utils_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestZeroOr(t *testing.T) {
-	assert.Equal(t, ZeroOr(0, 1), 1)
+	assert.Equal(t, 1, ZeroOr(0, 1))
 }
 
 func TestEmptyOr(t *testing.T) {
-	assert.Equal(t, EmptyOr([]int{}, []int{1}), []int{1})
+	assert.Equal(t, []int{1}, EmptyOr([]int{}, []int{1}))
 }
 
 func TestParseCookie(t *testing.T) {
-	maxAge := "MaxAge=3600;"
-	assert.Equal(t, "MaxAge=3600", CookieToString(ParseCookie(maxAge)))
+	maxAge := "Name=Test;MaxAge=3600;"
+	assert.Equal(t, "Name=Test; MaxAge=3600", CookieToString(ParseCookie(maxAge)))
 }
